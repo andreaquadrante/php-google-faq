70303feccbb067d6508a44ca10e38b130ee995de
Andrea <quadranteandrea@gmail.com>
2021-01-09 18:48:27 +0100

    adds navbar

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Domande frequenti - Privacy e termini - Google</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/969320eebd.js" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+
+    <nav>
+        <div class="nav_top">
+            <div class="logo">
+                <img src="img/google_logo.png" alt="Google">
+                <span>Privacy e termini</span>
+            </div>
+            <div class="menu">
+                <i class="fas fa-th"></i>
+                <div class="avatar"><img src="img/avatar_io.jpg" alt=""></div>
+            </div>
+        </div>
+        <div class="nav_bottom">
+            <ul>
+                <li>Introduzione</li>
+                <li>Norme sulla privacy</li>
+                <li>Termini di servizio</li>
+                <li>Tecnologie</li>
+                <li>Domande Frequenti</li>
+            </ul>
+        </div>
+    </nav>
+
+</body>
+
+</html>
